ad0d21a53eef28c7030d1ef8ae89e8d701d2ad52
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2021-07-26 21:10:51 +0800

    Update

--- Symmetric_Ternary_Logic.c
+++ Symmetric_Ternary_Logic.c
@@ -1,27 +1,50 @@
-int tNOT(int x)
+int tNOT(int x)//对称三进制非门
 {
-	if (x = 1){
+	if (x == 1){
 		return -1;
 	}
-	if (x = 0){
+	if (x == 0){
 		return 0;
 	}
-	if (x = -1){
+	if (x == -1){
 		return 1;
 	}
 }
-int tAND(int x, int y)
+int tAND(int x, int y)//对称三进制与门
 {
-	if (x = y){
+	if ((x == y) & (x != 0) & (y != 0)){
 		return 1;
 	}
-	if ((x != y) & (((x = 0) & (y != 0)) | (x != 0) & (y = 0))){
+	if ((x == 0) | (y == 0)){
 		return 0;
 	}
 	if ((x != y) & (x != 0) & (y != 0)){
 		return -1;
 	}
 }
-int tOR(int x, int y){
-
+int tOR(int x, int y)//对称三进制或门
+{
+	if (x + y == 0)
+	{
+		return 0;
+	}
+	if (((x == -1) & (y != 1)) | ((y == -1) & (x != 1))
+	{
+		return -1;
+	}
+	if (((x == 1) & (y != -1)) | ((y == 1) & (x != -1)))
+	{
+		return 1;
+	}
+}
+int tNAND(int x, int y)//对称三进制与非门
+{
+	if ((x == 1) & (y == 1))
+	{
+		return -1;
+	}
+	if ((x == 0) | (y == 0))
+	{
+		return 0;
+	}
 }
